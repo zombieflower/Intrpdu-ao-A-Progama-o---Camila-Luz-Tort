ed2a1fddd645029d9f7eb16241ba82ce66c025b6
Camila Luz Tort <camila.luz@escolar.ifrn.edu.br>
2026-08-21 10:23:12 -0300

    lalala

--- questoes1/q4.py
+++ questoes1/q4.py
@@ -1,7 +1,5 @@
 
-• A Condição: Leia a mensagem digitada pelo usuário. Usando a função len(), verifique o
-comprimento do texto:
-text = 
+text = 8
 if text < 3 or text > 140:
     print("Mensagem bloqueada por violar as diretrizes de spam!")
 else:
