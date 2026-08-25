30fcd81a0a8a95166abe3e774678964b2c75101f
Camila Luz Tort <camila.luz@escolar.ifrn.edu.br>
2026-08-25 09:58:21 -0300

    43

---
+++ questoes1/q7.py
@@ -0,0 +1,10 @@
+
+d = "miau"
+l = "4444"
+print("Dica: Nome de usuário = Letra minuscula, o som que o gato faz; Senha = 2222 * 2")
+rw = input("Nome de usuário: ")
+rgw = input("Senha do usuário: ")
+if rw == d and rgw == l:
+    print("Acesso liberado. Bem-vindo ao SUAP!")
+else: 
+    print("Credenciais inválidas. Acesso bloqueado!"  )
